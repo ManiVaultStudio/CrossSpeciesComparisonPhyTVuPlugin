5c7f000919ac73aaba279f6580fefd7946f48d08
Soumyadeep Basu <44787782+sbvis@users.noreply.github.com>
2025-09-12 13:40:55 +0200

    Update dependencies to use latest versions

--- conanfile.py
+++ conanfile.py
@@ -32,7 +32,7 @@ class CrossSpeciesComparisonPhyTVuPluginConan(ConanFile):
     default_options = {"shared": True, "fPIC": True}
 
     # Data plugin dependencies
-    requires = ("CrossSpeciesComparisonTreeData/cytosploreviewer@lkeb/stable", "CrossSpeciesComparisonTreeMetaData/cytosploreviewer@lkeb/stable")
+    requires = ("CrossSpeciesComparisonTreeData/latest@lkeb/stable", "CrossSpeciesComparisonTreeMetaData/latest@lkeb/stable")
 
     # Qt requirement is inherited from hdps-core
 
